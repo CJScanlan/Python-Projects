95d5974c9c09cfafbc09073292c738b1680f1f16
Carly Scanlan <carlyscanlan@gmail.com>
2020-11-03 10:38:47 -0800

    Create StringVariables.py
    Practice with basic syntax

---
+++ PythonPractice/StringVariables.py
@@ -0,0 +1,14 @@
+food = """Pumpkins are delicious,
+especially when used in pies!"""
+print(food)
+print(len(food))
+print(food.strip())
+print(food.upper())
+x = "kin" in food
+print(x)
+y = "apples" not in food
+print(y)
+sentence = food + " I love pumpkins. Tis the season!"
+print(sentence)
+txt = "I enjoy Pumpkin spiced lattes, or commonly referred to as \"PSL\"."
+print(txt)
