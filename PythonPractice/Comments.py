e533e4ea7d8d2ee87a5579f7f54dad83443c5e5f
Carly Scanlan <carlyscanlan@gmail.com>
2020-11-10 13:10:52 -0800

    Create new files

---
+++ PythonPractice/Comments.py
@@ -0,0 +1,17 @@
+
+#this is my note
+
+"""this is my note
+and this is still my note
+and this again is still my note.
+"""
+
+def printMe():
+    """this is my docstring
+    """
+
+    print('Meeeeeee!')
+
+print(printMe.__doc__)
+
+#in shell, type help(printMe) to find commentary 
