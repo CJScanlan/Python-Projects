3941a5c8b12b08c23245f8f506a2fd2a0a602094
Carly Scanlan <carlyscanlan@gmail.com>
2020-11-18 12:41:43 -0800

    Update files

--- Tkinter/Student Tracking/student_gui.py
+++ Tkinter/Student Tracking/student_gui.py
@@ -8,15 +8,15 @@ import student_func
 
 
 def load_gui(self):
-    self.lbl_fname = tk.Label(self.master, text="First Name:")
+    self.lbl_fname = tk.Label(self.master, text="First Name:", bg="lightblue")
     self.lbl_fname.grid(row=0, column=0, padx=(25,0), pady=(10,0), sticky=N+W)
-    self.lbl_lname = tk.Label(self.master, text="Last Name:")
+    self.lbl_lname = tk.Label(self.master, text="Last Name:", bg="lightblue")
     self.lbl_lname.grid(row=2, column=0, padx=(25,0), pady=(10,0), sticky=N+W)
-    self.lbl_phone = tk.Label(self.master, text="Phone Number:")
+    self.lbl_phone = tk.Label(self.master, text="Phone Number:", bg="lightblue")
     self.lbl_phone.grid(row=4, column=0, padx=(25,0), pady=(10,0), sticky=N+W)
-    self.lbl_email = tk.Label(self.master, text="Email Address:")
+    self.lbl_email = tk.Label(self.master, text="Email Address:", bg="lightblue")
     self.lbl_email.grid(row=6, column=0, padx=(25,0), pady=(10,0), sticky=N+W)
-    self.lbl_course = tk.Label(self.master, text="Current Course:")
+    self.lbl_course = tk.Label(self.master, text="Current Course:", bg="lightblue")
     self.lbl_course.grid(row=8, column=0, padx=(25,0), pady=(10,0), sticky=N+W)
 
 
@@ -32,18 +32,19 @@ def load_gui(self):
     self.txt_course.grid(row=9, column=0, rowspan=1,columnspan=2,padx=(30,40),pady=(0,0),sticky=N+E+W)
 
     #listbox with scrollbar
-    self.scrollbar1 = Scrollbar(self.master, orient=VERTICAL)
-    self.lstList1 = Listbox(self.master, exportselection=0,yscrollcommand=self.scrollbar1.set)
+    self.scrollbar1 = Scrollbar(self.master,orient=VERTICAL)
+    self.lstList1 = Listbox(self.master,exportselection=0,yscrollcommand=self.scrollbar1.set)
     self.lstList1.bind('<<ListboxSelect>>',lambda event: student_func.onSelect(self,event))
     self.scrollbar1.config(command=self.lstList1.yview)
-    self.scrollbar1.grid(row=1,column=5,rowspan=9,columnspan=1,padx=(0,0),pady=(0,0),sticky=N+E+S)
-    self.lstList1.grid(row=1,column=2,rowspan=9,columnspan=4,padx=(0,0),pady=(0,0),sticky=N+E+S+W)
+    self.scrollbar1.grid(row=1,column=6,rowspan=9,columnspan=1,padx=(0,0),pady=(0,0),sticky=N+E+S)
+    self.lstList1.grid(row=1,column=2,rowspan=9,columnspan=4,padx=(0,0),pady=(0,0),sticky=N+S+E+W)
+
 
     #buttons
     self.btn_submit = tk.Button(self.master,width=12,height=2,text='Submit',command=lambda: student_func.addToList(self))
     self.btn_submit.grid(row=10, column=0,padx=(25,0),pady=(45,10),sticky=W)
     self.btn_delete = tk.Button(self.master,width=12,height=2,text='Delete',command=lambda: student_func.onDelete(self))
-    self.btn_delete.grid(row=10, column=1,padx=(25,0),pady=(45,10),sticky=W)
+    self.btn_delete.grid(row=10, column=5,padx=(25,0),pady=(45,10),sticky=E)
 
     student_func.create_db(self)
     student_func.onRefresh(self)
